383fcd00a90113b0af60448b5f36e59cc8aca410
Austin <austin@Madisons-MBP-2.hsd1.ut.comcast.net>
2022-12-27 11:41:56 -0700

    check

---
+++ model.py
@@ -0,0 +1,68 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"User id={self.id} email{self.email}"
+
+class Book(db.Model):
+
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    overview = db.Column(db.String)
+
+    def __repr__(self):
+        return f"Book title {self.title}"
+
+class Users_book(db.Model):
+
+    __tablename__ = "users_books"
+
+    id = db.Column(db.Integer, autoincrement=True, primaryu_key=True)
+    users_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    books_id = db.Column(db.Integer, db.ForeignKey("books.id"))
+    pages_read = db.Column(db.Integer)
+    currently_reading = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f"Users_book id{self.id}"
+
+
+class Thought(db.Model):
+
+    __tablename__ = "thoughts"
+
+    users_books_id = db.Column(db.Integer, db.ForeignKey("users_books.id"))
+    thoughts = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"Thought thoughts{self.thoughts}"
+
+
+
+def connect_to_db(flask_app, db_uri=os.environ["DATABASE_URI"], echo=False):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app = flask_app
+    print("Connected to db")
+
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
