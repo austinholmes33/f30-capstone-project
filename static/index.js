ff45469227f2913bdfc7842143298d865ccf15e3
Austin Holmes <austin.holmes4@gmail.com>
2023-04-10 15:19:04 -0600

    commented out JS file for now, taking the direction of using Python for progress bar functionality instead

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
-var i = 0;
+// var i = 0;
 
-var percent = book.pages_read
-var total = book.book.pages
+// var percent = book.pages_read
+// var total = book.book.pages
 
 // function progress() {
     // if (percent === total) {
@@ -9,22 +9,22 @@ var total = book.book.pages
     // }
 // }
 
-function progress() {
-    if (i == 0) {
-        i = 1;
-        var elem = document.getElementById("progress-bar");
-        var width = 10
-        var id = setInterval(frame, 10);
-        function frame() {
-            if (width >= 100) {
-                clearInterval(id);
-                i = 0;
-            } else {
-                width++;
-                elem.style.width = width + "%";
-                elem.innterHTML = width + "%";
-            }
-        }
-    }
+// function progress() {
+//     if (i == 0) {
+//         i = 1;
+//         var elem = document.getElementById("progress-bar");
+//         var width = 10
+//         var id = setInterval(frame, 10);
+//         function frame() {
+//             if (width >= 100) {
+//                 clearInterval(id);
+//                 i = 0;
+//             } else {
+//                 width++;
+//                 elem.style.width = width + "%";
+//                 elem.innterHTML = width + "%";
+//             }
+//         }
+//     }
 
-}
+// }
